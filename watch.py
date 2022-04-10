124425f9d0cd743068dba8b568ef65703fc95fe0
shrihari1999 <shrihari1999@gmail.com>
2022-04-11 02:39:17 +0530

    feat: Working prototype

--- watch.py
+++ watch.py
@@ -1,8 +1,11 @@
 from mss import mss, tools as mss_tools
 from pathlib import Path
-import os, sys, time
+import os, sys, time, cv2, numpy as np
+from tensorflow import keras
+from collections import deque
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
+
 def set_system_volume(volume):
     os_name = sys.platform
     if os_name == 'win32':
@@ -11,12 +14,48 @@ def set_system_volume(volume):
     elif os_name == 'linux':
         os.system(f"pactl set-sink-volume 1 {volume}%")
 
+box = {"top": 85, "left": 255, "width": 110, "height": 90}
+labels = ['ipl', 'kkr', 'lsg', 'pbks', 'rr', 'srh']
+img_size = 90
+model = keras.models.load_model('./ipl_model')
+
+muted = False
+prediction_queue = deque(maxlen=2)
+max_reached = False
+
 time.sleep(5)
 with mss() as sct:
-    monitor = sct.monitors[0]
-    box = {"top": 85, "left": 255, "width": 110, "height": 90}
-    for i in range(0, 60):
+    while True:
         sct_img = sct.grab(box)
-        mss_tools.to_png(sct_img.rgb, sct_img.size, output=f"result{i+1}.png")
-        time.sleep(2)
-    set_system_volume(0)
+        png = mss_tools.to_png(sct_img.rgb, sct_img.size)
+        nparr = np.frombuffer(png, np.uint8)
+        img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)[...,::-1] #convert BGR to RGB format
+        resized_arr = cv2.resize(img_np, (img_size, img_size)) # Reshaping images to preferred size
+        x_val = np.array([resized_arr]) / 255
+        x_val.reshape(-1, img_size, img_size, 1)
+        predictions = model.predict(x_val)[0]
+        prediction_queue.append(max(predictions) > 0.9)
+        if max_reached:
+            if len(set(prediction_queue)) == 1:
+                if prediction_queue[0]:
+                    if muted:
+                        set_system_volume(50)
+                        muted = False
+                    print('logo', max(predictions))
+                else:
+                    if not muted:
+                        set_system_volume(0)
+                        muted = True
+                    print('break', max(predictions))
+        else:
+            max_reached = len(prediction_queue) == prediction_queue.maxlen
+        time.sleep(0.5)
+
+# time.sleep(5)
+# with mss() as sct:
+#     monitor = sct.monitors[0]
+#     for i in range(0, 60):
+#         sct_img = sct.grab(box)
+#         mss_tools.to_png(sct_img.rgb, sct_img.size, output=f"result{i+1}.png")
+#         time.sleep(2)
+#     set_system_volume(0)
